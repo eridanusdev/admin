772ac27e8a39e764d5488ebdb4ec980e00bbb31a
Eridanus-ui <njigupaul22@gmail.com>
2024-11-08 15:16:46 +0300

    chore: Update Navbar component
    Update Navbar with new styling and remove unused asset.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { assets } from "../assets/assets";
 
 export default function Navbar() {
   return (
-    <div>
-      <img src={assets.logo} alt="logo" className="w-[max(10%, 80px)]" />
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img src={assets.logo} alt="logo" className="w-[max(10%,80px)]" />
       <button className="bg-gray-600 text-white px-5 sm:px-7 sm:py-2 rounded-full font-muktaVaani">
         Logout
       </button>
