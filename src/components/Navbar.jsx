d83abbe53924d05685bab8bf23e63c210ba07843
Eridanus-ui <njigupaul22@gmail.com>
2024-11-09 08:35:07 +0300

    feat: Improve navbar styling
    Update logo size and button styling for a more consistent
    visual experience.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,11 @@ import { assets } from "../assets/assets";
 export default function Navbar({ setToken }) {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img src={assets.logo} alt="logo" className="w-[max(10%,80px)]" />
+      <div className="">
+        <img src={assets.logo} alt="logo" className="w-[max(10%,90px)]" />
+      </div>
       <button
-        className="bg-gray-600 text-white px-5 sm:px-7 sm:py-2 rounded-full font-muktaVaani"
+        className="bg-gray-600 text-white py-1 px-5 sm:px-7 sm:py-2 rounded-xl font-muktaVaani"
         onClick={() => setToken("")}
       >
         Logout
